4d54a257c931cef78f34bcef483c7e5c98d126a4
YiYaYiYaHei <1823972245@qq.com>
2021-06-07 09:35:27 +0800

    feature--添加文件上传下载接口

--- NodeService/app.js
+++ NodeService/app.js
@@ -1,30 +1,33 @@
 const express = require('express'),
-    app = express(),
-    bodyParser = require('body-parser'),
-    path = require('path'),
-    port = process.env.PORT || 13666,
-    request = require('./dbtpl/request'),
-    formReq = require('./dbtpl/formReq'),
-    socket = require('./dbtpl/socket');
+  app = express(),
+  bodyParser = require('body-parser'),
+  path = require('path'),
+  port = process.env.PORT || 13666,
+  request = require('./dbtpl/request'),
+  formReq = require('./dbtpl/formReq'),
+  fileReq = require('./dbtpl/fileReq'),
+  socket = require('./dbtpl/socket');
 
 /* 设置启动静态资源路径和请求编码及转义 */
 app.use(express.static(path.join(__dirname, '/static')));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
 /* 服务启动监听 */
-const server = app.listen(port, function(){
-    console.log('customize system service starting on port ' + port);
+const server = app.listen(port, function () {
+  console.log('customize system service starting on port ' + port);
 });
 
 /* 设置CORS跨域 */
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With,token");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1');
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With,token");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1');
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
 });
 
 /* 自定义大屏数据请求 */
@@ -32,6 +35,9 @@ request(app);
 
 /* 自定义表单接口请求 */
 formReq(app);
+
+// 文件上传、下载
+fileReq(app);
 
 /* socket 接入 */
 socket(app, server);
